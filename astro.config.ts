d7f545140accf7ec472c39e146785c6c61f65efe
paulrudy <1110792+paulrudy@users.noreply.github.com>
2025-08-21 12:37:46 -0700

    commit

---
+++ astro.config.ts
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig, envField } from 'astro/config';
+
+import netlify from '@astrojs/netlify';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: netlify({
+    devFeatures: {
+      images: false,
+      environmentVariables: false,
+    },
+  }),
+  env: {
+    schema: {
+      MY_SECRET_VALUE: envField.string({ context: 'server', access: 'secret' }),
+      MY_NON_SECRET_VALUE: envField.string({
+        context: 'client',
+        access: 'public',
+      }),
+    },
+  },
+});
